9b327478fe7a720405dbbcf861b0c73234cbf69c
praveen2007-VY <praveenr2007440@gmail.com>
2026-01-20 23:59:27 +0530

    update logo in website plus backend update

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://your-frontend.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // mongoose.connect("mongodb://127.0.0.1:27017/CRUD")
@@ -17,8 +22,8 @@ app.use(express.json());
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch((err) => console.log("MongoDB Error ❌", err));
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log("MongoDB Error ", err));
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
@@ -433,7 +438,8 @@ app.put('/runwaystatus/:id', async (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
